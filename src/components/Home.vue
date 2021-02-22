5c9df3c28b24c69ad7dc2ad7260976781fa72aaf
陈童 <1421911991@qq.com>
2021-02-22 10:26:23 +0800

    完善登陆后的操作

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    HOME
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data () {
+    return {
+    }
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
--- src/components/login.vue
+++ src/components/login.vue
@@ -55,6 +55,9 @@ export default {
         // console.log(res)
         if (res.meta.status !== 200) return this.$message.error('登陆失败')
         this.$message.success('登陆成功')
+        // console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
       })
     }
   }
