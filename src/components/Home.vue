dc98de7d6d41dfb020de735bf7b5f8129be276c8
陈童 <1421911991@qq.com>
2021-02-22 14:11:08 +0800

    侧边栏梳理

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,24 @@
     </el-header>
     <!-- 页面主体 -->
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <!-- 一级菜单 -->
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
