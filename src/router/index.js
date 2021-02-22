be394d77bbcb925fb06bf85297020c1e5cd760c4
陈童 <1421911991@qq.com>
2021-02-22 10:48:50 +0800

    路由守卫控制访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,14 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  // 如果访问的是 登陆 页面，则放行，允许访问
+  if (to.path == '/login') return next()
+
+  const tokenStr = window.sessionStorage.getItem('token')
+  // 如果没有获取到token值的话强制跳转到登陆页面
+  if(!tokenStr) return next('/login')
+  next()
+})
+
 export default router
