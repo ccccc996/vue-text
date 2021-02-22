595c6ffc4e266f505b93deb4c233f394d4c2fc4a
陈童 <1421911991@qq.com>
2021-02-22 16:15:28 +0800

    渲染侧边栏信息

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,17 +12,17 @@
     <el-container>
       <el-aside width="200px">
         <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
-          <el-submenu index="1">
+          <el-submenu :index="item.id" v-for="item in menulist" :key="item.id">
             <!-- 一级菜单 -->
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <span>{{ item.authName }}</span>
             </template>
             <!-- 二级菜单 -->
-            <el-menu-item index="1-4-1">
+            <el-menu-item :index="subitem.id" :key="subitem.id" v-for="subitem in item.children">
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>导航一</span>
+                <span>{{ subitem.authName }}</span>
               </template>
             </el-menu-item>
           </el-submenu>
