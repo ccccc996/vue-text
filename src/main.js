884de0e190a6847766113f4cb1463b23307d0ecf
陈童 <1421911991@qq.com>
2021-02-22 14:42:33 +0800

    设置请求拦截器和响应拦截器

--- src/main.js
+++ src/main.js
@@ -10,6 +10,22 @@ import './assets/css/global.css'
 //引入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
+// 请求拦截器
+axios.interceptors.request.use((config) => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+
+// 响应拦截器
+axios.interceptors.response.use((res) => {
+  // console.log(res)
+  if(res.data.meta.msg === '无效token' && res.data.meta.status === 400){
+    location.href = '/#/login'
+  }
+  return res
+})
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
